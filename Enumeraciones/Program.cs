b9c5745e2093e463bd581debe48a12470f5a3dfc
KevinMuno <kevindediosant@gmail.com>
2023-08-31 12:03:42 -0600

    final

--- Enumeraciones/Program.cs
+++ Enumeraciones/Program.cs
@@ -1,11 +1,23 @@
 ﻿int estatusOperaciones = 5;
 
-if (estatusOperaciones == 1)
+if (estatusOperaciones == (int)EstatusOperacion.Exitoso) 
 {
 
     //....
 }
-else if (estatusOperaciones == 2)
+else if (estatusOperaciones == (int)EstatusOperacion.ClienteNoEncontrado)
 {
     //....
+}
+else if (estatusOperaciones == (int)EstatusOperacion.ErrorDeSistema)
+{
+    //...
+}
+
+public enum EstatusOperacion
+{
+    Exitoso =1,
+    ClienteNoEncontrado= 2,
+    ErrorDeSistema = 5,
+
 }
